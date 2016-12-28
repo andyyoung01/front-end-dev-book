6a2654d1f4848a54e68f21413c55dd360854360c
andyyoung01 <andyyoung01@gmail.com>
2016-12-28 08:44:17 +0800

    finished chapter11

--- coffeerun/scripts/formhandler.js
+++ coffeerun/scripts/formhandler.js
@@ -24,6 +24,7 @@
                     console.log(item.name + ' is ' + item.value);
                 });
                 console.log(data);
+
                 fn(data);
                 this.reset();
                 this.elements[0].focus();
